f7d68fbd572437466272ddd5d22b132630b2a922
Ambaliya_Rahul <141474810+Amabaliyarahul@users.noreply.github.com>
2023-11-06 17:39:26 +0530

    Create p3.cpp

---
+++ p3.cpp
@@ -0,0 +1,28 @@
+/* AMBALIYA RAHUL */
+/*220130318104*/
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    ifstream count("test.txt");
+
+    if (!count.is_open())
+    {
+        cout << "Failed to open the file." << endl;
+        return 1;
+    }
+
+    char ch;
+    int count_ch = 0;
+    while (count.get(ch))
+    {
+        count_ch++;
+    }
+
+    count.close();
+
+    cout << "Number of character in text.txt is ::" << count_ch << endl;
+
+    return 0;
+}
